d0266317a7db2cb37b508fc9baaaf00ee30314f9
usmansbk <usmansbk@gmail.com>
2022-07-27 02:56:25 +0100

    fix: Jimp buffer error from url

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
-import Jimp = require("jimp");
+import Jimp from "jimp";
+import axios from "axios";
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -9,7 +10,8 @@ import Jimp = require("jimp");
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string> {
-  const photo = await Jimp.read(inputURL);
+  const response = await axios.get(inputURL, { responseType: "arraybuffer" });
+  const photo = await Jimp.read(response.data);
   const outpath = `${__dirname}/tmp/filtered.${Math.floor(
     Math.random() * 2000
   )}.${photo.getExtension()}`;
