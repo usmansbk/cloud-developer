2d4991c34286de67ca4396d51355610a5bdbba77
usmansbk <usmansbk@gmail.com>
2022-08-05 17:57:28 +0100

    add stubs

--- course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/helpers/todos.ts
@@ -1,5 +1,5 @@
 import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
+import { AttachmentUtils } from './attachmentUtils'
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
@@ -8,3 +8,24 @@ import * as uuid from 'uuid'
 import * as createError from 'http-errors'
 
 // TODO: Implement businessLogic
+
+const logger = createLogger(process.env.APP_NAME)
+
+export const getTodosForUser = async (id: string) => {
+  logger.info(id)
+}
+
+export const createTodo = async (input: CreateTodoRequest, userId: string) => {
+  logger.info({ ...input, userId, id: uuid() })
+}
+
+export const updateTodo = async (id: string, input: UpdateTodoRequest) => {
+  logger.info({
+    id,
+    ...input
+  })
+}
+
+export const deleteTodo = async (id: string) => {
+  logger.info(id)
+}
